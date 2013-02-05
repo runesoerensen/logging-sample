bed2b9d98951061d1fa362aeaf48976fff68c1df
Rune <rs@appharbor.com>
2013-02-04 18:30:13 -0800

    Initial commit

---
+++ LoggingApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NLog;
+using System.Threading;
+
+namespace LoggingApp
+{
+	class Program
+	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				logger.Warn("Successfully logged from \"{0}\"", Environment.MachineName);
+				Thread.Sleep(30000);
+			}
+		}
+	}
+}
